27f03748b072ca968b42daaeba629a29aab7769c
luiscastromallma <47374020+luiscastromallma@users.noreply.github.com>
2019-03-11 11:44:35 -0500

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+mix.options({
+    processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+});
+
+mix.js('resources/js/ecommerce.js', 'public/js')
+   .sass('resources/sass/ecommerce/ecommerce.sass', 'public/css');
